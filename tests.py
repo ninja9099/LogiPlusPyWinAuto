12d6f54e801656b9b2c32354de40fbb07b7e7bc0
KP <kamanipankaj9099@gmail.com>
2023-11-14 13:44:46 +0530

    Working tests.py

--- tests.py
+++ tests.py
@@ -9,6 +9,9 @@ main_window = app.LogioOptionsPlus.child_window(title="ADD DEVICE", control_t
 
 main_window.click_input()
 
+bluetooth_button = app.LogioOptionsPlus.child_window(title="receiver-icon Bluetooth ", auto_id="Bluetooth-connection-info-container", control_type="Button").wrapper_object()
 # print control identifier of the new window by keeping the that window open
 # so the control identifer are current state for the application
+bluetooth_button.click_input()
+
 app.LogiOptionsPlus.print_control_identifiers()
