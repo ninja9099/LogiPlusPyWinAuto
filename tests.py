0ebbe45882d85d4b6dc919a56bcfc1af5a06bc56
KP <kamanipankaj9099@gmail.com>
2024-02-15 17:47:53 +0530

    Added two step automation for demonstration purpose

--- tests.py
+++ tests.py
@@ -8,9 +8,21 @@ main_window = app['CPU-Z Dialog']
 main_window.wait('visible', 10)
 main_window.draw_outline()
 
-main_window.print_control_identifiers()
+# main_window.print_control_identifiers()
 
-import pdb
-pdb.set_trace()
+# main_window.ok.click()
+# Steps
+# 1 click on validate button to open a validate dialouge
+main_window.Validate.click()
 
-main_window.ok.click()
+
+# main_window.print_control_identifiers()
+
+# 2 get the pane in new window
+new_pan = main_window["What is ValidationPane"]
+new_pan.wait('visible', 10)
+
+
+checkbox = new_pan.child_window(title=" Private validation", auto_id="1261", control_type="CheckBox")
+checkbox.click_input()
+new_pan.Close.click()
